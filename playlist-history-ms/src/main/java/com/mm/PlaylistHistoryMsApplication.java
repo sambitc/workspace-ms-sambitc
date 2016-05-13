97c1000f81ee1beaf835cd34a4fddbd4c4fb29ea
sambitc <sambitgudu@yahoo.co.in>
2016-05-13 17:56:10 +0530

    implemented hystrix

--- playlist-history-ms/src/main/java/com/mm/PlaylistHistoryMsApplication.java
+++ playlist-history-ms/src/main/java/com/mm/PlaylistHistoryMsApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringCloudApplication
-@SpringBootApplication
-@EnableDiscoveryClient
+@EnableFeignClients
+@ComponentScan(basePackages = { "com.mm.config", "com.mm.data", "com.microservices.data","com.mm.controller"})
 public class PlaylistHistoryMsApplication {
 
 	public static void main(String[] args) {
